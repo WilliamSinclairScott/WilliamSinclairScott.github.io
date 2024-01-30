8887a70a8bd2b8a23189573fb21308bf08b6e56e
William Scott <william.sinclair.scott@gmail.com>
2024-01-30 14:49:28 -0500

    Attempting to add Routing

---
+++ src/templates/Home.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+export default function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <center>
+                    This is the Homepage
+                </center>
+            </header>
+        </div>
+    )
+}
