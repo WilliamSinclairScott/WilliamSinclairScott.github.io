93ba01cdb55cff765d7b52a2b916d0cef843876d
William Scott <william.sinclair.scott@gmail.com>
2024-02-05 21:24:04 -0500

    changing focus to create an outline with some css.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.js";
 import About from "./pages/About/About";
 import Example from "./pages/Example/Example";
-import Navbar from "./Navbar";
+import { DropdownMenu, Navbar, NavItem } from "./Navbar.js";
+import { ReactComponent as ArrowIcon } from './images/icons/arrow.svg';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar>
+        <NavItem icon={<ArrowIcon />} />
+        <NavItem icon="Y" />
+        <NavItem icon="Z" />
+
+        <NavItem icon="A'">
+          <DropdownMenu>
+
+          </DropdownMenu>
+        </NavItem>
+      </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,57 @@
-import { Link } from "react-router-dom";
-import './App.css';
+import { ClassNames } from '@emotion/react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as CogIcon } from './images/icons/cog.svg';
+import { ReactComponent as PlusIcon } from './images/icons/plus.svg';
 
-export default function Navbar() {
+export function Navbar(props) {
     return (
-        <div className="App">
-            <center>
-                <ul>
-                    <Link to="/">Home</Link>
-                    <Link to="/About">About</Link>
-                    <Link to="/Example">Example</Link>
-                </ul>
-            </center>
-        </div>
-
+        <nav className="navbar">
+            <ul className="navbar-nav"> {props.children}</ul>
+        </nav>
     )
 }
+
+export function NavItem(props) {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <li className="nav-item">
+            <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+                {props.icon}
+            </a>
+
+            {open && props.children}
+        </li>
+    )
+}
+
+export function DropdownMenu() {
+
+    function DropdownItem(props) {
+        return (
+            <a href="#" className="menu-item">
+                <span className='icon-button'>{props.leftIcon}</span>
+
+                {props.children}
+
+                <span className='icon-right'>{props.rightIcon}</span>
+            </a>
+        )
+    }
+
+    return (
+        <div className='dropdown'>
+            <DropdownItem>Example test</DropdownItem>
+            <DropdownItem
+                leftIcon={<CogIcon />}
+                rightIcon={<PlusIcon />}>
+                Better Example
+            </DropdownItem>
+        </div>
+    );
+}
+
+
+export default Navbar;
